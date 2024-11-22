5e8e3cac16cc5ea6fbb01cdc8f4fe119a63ed589
Cristian Quito <cristianandresquito@gmail.com>
2024-11-21 21:09:26 -0500

    feat fix post

--- lib/presentation/screens/3_posts/widgets/step_barrel.dart
+++
@@ -1,4 +0,0 @@
-export 'package:chambeape/presentation/screens/3_posts/widgets/steps/confirm_step.dart';
-export 'package:chambeape/presentation/screens/3_posts/widgets/steps/details_step.dart';
-export 'package:chambeape/presentation/screens/3_posts/widgets/steps/location_step.dart';
-export 'package:chambeape/presentation/screens/3_posts/widgets/steps/settting_step.dart';
